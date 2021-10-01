c5811dff757f7033fae1515cdf79cb7ab8097898
Ronja Koistinen <ronja.koistinen@helsinki.fi>
2021-10-01 15:21:15 +0300

    Add a bit of sleep the shibd's start script

--- shibd/start.sh
+++ shibd/start.sh
@@ -7,3 +7,5 @@ ln -sf -t /etc/shibboleth/ /shib-config/*
 tail -F -c +0 /var/log/shibboleth/shibd.log 2> /dev/null &
 # Start shibd
 /usr/sbin/shibd -F
+# Sleep for a bit after shibd dies so that tail has time to print all of the log
+sleep 15
